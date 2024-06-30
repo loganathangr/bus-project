6fb240584230f8986a59a44e9c24c1df0f0d9ec3
Loganathan G R <loganathangr77@gmail.com>
2024-06-30 14:43:01 +0530

    Add files via upload

---
+++ src/main/java/com/shivajivarma/brs/utility/IOHelpers.java
@@ -0,0 +1,53 @@
+package com.shivajivarma.brs.utility;
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author <a href="http://shivajivarma.com" target="_blank">Shivaji Varma</a>
+ */
+public class IOHelpers {
+
+	public static void printHTML(String html, String pageName) {
+		FileWriter fw;
+		try {
+			fw = new FileWriter(System.getenv("APPDATA") + "\\" + pageName
+					+ ".html");
+			fw.write(html);
+			fw.close();
+			Runtime.getRuntime().exec(
+					"rundll32 url.dll,FileProtocolHandler file:///"
+							+ System.getenv("APPDATA") + "/" + pageName
+							+ ".html");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static String getFileAsString(String filePath) {
+		String content = new String("");
+		try {
+			InputStream in = IOHelpers.class.getClassLoader().getResourceAsStream(
+					filePath);
+			BufferedReader input = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+
+			String line;
+			while ((line = input.readLine()) != null) {
+				content = content + line;
+			}
+			input.close();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return content;
+	}
+}
